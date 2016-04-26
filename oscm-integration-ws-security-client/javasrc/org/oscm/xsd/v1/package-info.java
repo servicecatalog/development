38ffd4ef0eb467ce8b2bb3d1efb7aa62113f11e1
miethaner <miethaner@estmiethaner.intern.est.fujitsu.com>
2016-04-26 13:51:15 +0200

    deleted unused projects - Suzana

--- oscm-integration-ws-security-client/javasrc/org/oscm/xsd/v1/package-info.java
+++
@@ -1,7 +0,0 @@
-/*******************************************************************************
- *  Copyright FUJITSU LIMITED 2016 
- *******************************************************************************/
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://oscm.org/xsd")
-package org.oscm.xsd.v1;
-
