c40c67ebd974d335016a0687c865054f5940765c
Sven Kulle <sven.kulle@est.fujitsu.com>
2016-04-27 14:36:38 +0200

    started to implement snapshot operation

--- oscm-app-vmware/javasrc/org/oscm/app/vmware/remote/vmware/VMwareClient.java
+++ oscm-app-vmware/javasrc/org/oscm/app/vmware/remote/vmware/VMwareClient.java
@@ -30,129 +30,138 @@ import com.vmware.vim25.VimService;
  */
 public class VMwareClient implements AutoCloseable {
 
-	private static final Logger logger = LoggerFactory.getLogger(VMwareClient.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(VMwareClient.class);
 
-	private String url;
-	private String user;
-	private String password;
-	private ServiceConnection connection;
+    private String url;
+    private String user;
+    private String password;
+    private ServiceConnection connection;
 
-	public VMwareClient() {
+    public VMwareClient() {
 
-	}
+    }
 
-	public VMwareClient(VMwareCredentials credentials) {
-		this.url = credentials.getURL();
-		this.user = credentials.getUserId();
-		this.password = credentials.getPassword();
+    public VMwareClient(VMwareCredentials credentials) {
+        this.url = credentials.getURL();
+        this.user = credentials.getUserId();
+        this.password = credentials.getPassword();
+        logger.debug(
+                "Created VMware client for url " + url + " and user " + user);
+    }
 
-		logger.debug("Created VMware client for url: " + url + " and user: " + user);
-	}
+    /**
+     * Establish a connection to the vCenter.
+     */
+    public void connect() throws Exception {
 
-	/**
-	 * Establish a connection to the vCenter.
-	 */
-	public void connect() throws Exception {
+        // FIXME what to do?
+        HostnameVerifier hv = new HostnameVerifier() {
+            @Override
+            public boolean verify(String urlHostName, SSLSession session) {
+                return true;
+            }
+        };
 
-		// FIXME what to do?
-		HostnameVerifier hv = new HostnameVerifier() {
-			@Override
-			public boolean verify(String urlHostName, SSLSession session) {
-				return true;
-			}
-		};
+        int numFailedLogins = 0;
+        boolean repeatLogin = true;
 
-		int numFailedLogins = 0;
-		boolean repeatLogin = true;
+        while (repeatLogin) {
+            try {
 
-		while (repeatLogin) {
-			try {
+                HttpsURLConnection.setDefaultHostnameVerifier(hv);
 
-				HttpsURLConnection.setDefaultHostnameVerifier(hv);
+                VimService vimService = new VimService();
+                VimPortType vimPort = vimService.getVimPort();
+                Map<String, Object> ctxt = ((BindingProvider) vimPort)
+                        .getRequestContext();
 
-				VimService vimService = new VimService();
-				VimPortType vimPort = vimService.getVimPort();
-				Map<String, Object> ctxt = ((BindingProvider) vimPort).getRequestContext();
+                ctxt.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+                ctxt.put(BindingProvider.SESSION_MAINTAIN_PROPERTY,
+                        Boolean.TRUE);
 
-				ctxt.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
-				ctxt.put(BindingProvider.SESSION_MAINTAIN_PROPERTY, Boolean.TRUE);
+                ManagedObjectReference morSvcInstance = new ManagedObjectReference();
+                morSvcInstance.setType("ServiceInstance");
+                morSvcInstance.setValue("ServiceInstance");
+                ServiceContent serviceContent = vimPort
+                        .retrieveServiceContent(morSvcInstance);
+                vimPort.login(serviceContent.getSessionManager(), user,
+                        password, null);
+                connection = new ServiceConnection(vimPort, serviceContent);
+                repeatLogin = false;
+            } catch (Exception e) {
+                logger.error(
+                        "Failed to login. URL: " + url + " UserId: " + user, e);
+                if (numFailedLogins > 2) {
+                    throw e;
+                }
+                numFailedLogins++;
+                repeatLogin = true;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
 
-				ManagedObjectReference morSvcInstance = new ManagedObjectReference();
-				morSvcInstance.setType("ServiceInstance");
-				morSvcInstance.setValue("ServiceInstance");
-				ServiceContent serviceContent = vimPort.retrieveServiceContent(morSvcInstance);
-				vimPort.login(serviceContent.getSessionManager(), user, password, null);
-				connection = new ServiceConnection(vimPort, serviceContent);
-				repeatLogin = false;
-			} catch (Exception e) {
-				logger.error("Failed to login. URL: " + url + " UserId: " + user, e);
-				if (numFailedLogins > 2) {
-					throw e;
-				}
-				numFailedLogins++;
-				repeatLogin = true;
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException ex) {
-					Thread.currentThread().interrupt();
-				}
-			}
-		}
-	}
+    /**
+     * Returns whether the connection has been established
+     * 
+     * @return true if we are connected
+     */
+    public boolean isConnected() {
+        if (connection == null) {
+            return false;
+        }
 
-	/**
-	 * Returns whether the connection has been established
-	 * 
-	 * @return true if we are connected
-	 */
-	public boolean isConnected() {
-		boolean isAuthenticated = false;
-		if( connection != null ){
-			try {
-				ManagedObjectReference rootFolder = connection.getServiceContent().getRootFolder();
-		        getServiceUtil().getDynamicProperty(rootFolder, "name");
-				isAuthenticated = true;
-			} catch (Exception e) {
-				logger.debug("vSphere connection not valid");
-				isAuthenticated = false;
-			}
-			logger.debug("isAuthenticated: " + isAuthenticated);
-		}
-		return (connection != null && isAuthenticated);
-	}
+        try {
+            ManagedObjectReference rootFolder = connection.getServiceContent()
+                    .getRootFolder();
+            getServiceUtil().getDynamicProperty(rootFolder, "name");
+            return true;
+        } catch (@SuppressWarnings("unused") Exception e) {
+            return false;
+        }
+    }
 
-	/**
-	 * Returns the {@link ManagedObjectAccessor} based on the current client
-	 * connection.
-	 * 
-	 * @return the managed object accessor
-	 * @throws IllegalStateException
-	 *             when client is not connected
-	 */
-	public ManagedObjectAccessor getServiceUtil() {
-		return new ManagedObjectAccessor(getConnection());
-	}
+    public VimPortType getService() {
+        return getConnection().getService();
+    }
 
-	/**
-	 * Returns the {@link ServiceConnection} when client is in connect state.
-	 * 
-	 * @return the service connection
-	 * @throws IllegalStateException
-	 *             when client is not connected
-	 */
-	public ServiceConnection getConnection() {
-		if (connection == null) {
-			throw new IllegalStateException("Not connected");
-		}
-		return connection;
-	}
+    /**
+     * Returns the {@link ManagedObjectAccessor} based on the current client
+     * connection.
+     * 
+     * @return the managed object accessor
+     * @throws IllegalStateException
+     *             when client is not connected
+     */
+    public ManagedObjectAccessor getServiceUtil() {
+        return new ManagedObjectAccessor(getConnection());
+    }
 
-	@Override
-	public void close() throws Exception {
-		if (connection != null) {
-			connection.disconnect();
-		}
-		connection = null;
-	}
+    /**
+     * Returns the {@link ServiceConnection} when client is in connect state.
+     * 
+     * @return the service connection
+     * @throws IllegalStateException
+     *             when client is not connected
+     */
+    public ServiceConnection getConnection() {
+        if (connection == null) {
+            throw new IllegalStateException("Not connected to vCenter");
+        }
+        return connection;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.disconnect();
+        }
+        connection = null;
+    }
 
 }
