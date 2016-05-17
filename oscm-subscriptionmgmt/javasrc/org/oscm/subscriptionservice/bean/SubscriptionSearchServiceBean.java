e1fb9f74a9c547b9be41a1c3bc6b49c17c89b34b
Dawid Chojnacki <Dawid.Chojnacki@ts.fujitsu.com>
2016-05-17 12:02:36 +0200

    Revert: Bug #12675 - fix search custom attributes for subscriptions from different vendors - Lorenz

--- oscm-subscriptionmgmt/javasrc/org/oscm/subscriptionservice/bean/SubscriptionSearchServiceBean.java
+++ oscm-subscriptionmgmt/javasrc/org/oscm/subscriptionservice/bean/SubscriptionSearchServiceBean.java
@@ -60,33 +60,28 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
                 for (int i = 0; i < split.length; i++) {
                     String singleString = split[i];
                     singleString = singleString.trim();
-                    if (singleString.length() == 0) {
+                    if(singleString.length() == 0) {
                         continue;
                     }
                     org.apache.lucene.search.Query query = getLuceneQueryForFields(
                             singleString, getSearchFieldsForSubscription()[0]);
                     runResult = searchSubscriptionViaLucene(query, fts);
-                    logger.logDebug("I have found " + voList.size()
-                            + " subscriptions by referenceId");
+                    logger.logDebug("I have found " + voList.size() + " subscriptions by referenceId");
 
-                    query = getLuceneQueryForFields(singleString,
-                            getSearchFieldsForSubscription()[1]);
+                    query = getLuceneQueryForFields(
+                            singleString, getSearchFieldsForSubscription()[1]);
                     runResult.addAll(searchSubscriptionViaLucene(query, fts));
-                    logger.logDebug("I have found "
-                            + voList.size()
-                            + " subscriptions by referenceId and subscriptionId");
+                    logger.logDebug("I have found " + voList.size() + " subscriptions by referenceId and subscriptionId");
 
-                    query = getLuceneQueryForFields(singleString,
-                            getSearchFieldsForParameter());
+                    query = getLuceneQueryForFields(
+                            singleString, getSearchFieldsForParameter());
                     runResult.addAll(searchParametersViaLucene(query, fts));
-                    logger.logDebug("I have found " + voList.size()
-                            + " subscriptions by parameters value");
+                    logger.logDebug("I have found " + voList.size() + " subscriptions by parameters value");
 
-                    query = getLuceneQueryForFields(singleString,
-                            getSearchFieldsForUda());
+                    query = getLuceneQueryForFields(
+                            singleString, getSearchFieldsForUda());
                     runResult.addAll(searchUdasViaLucene(query, fts));
-                    logger.logDebug("I have found " + voList.size()
-                            + " subscriptions by uda value");
+                    logger.logDebug("I have found " + voList.size() + " subscriptions by uda value");
 
                     if (i == 0) {
                         voList.addAll(runResult);
@@ -106,10 +101,8 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
 
     private Set<Long> findCommonIds(Set<Long> voList, Set<Long> runResult) {
         Set<Long> result = new HashSet<>();
-        Set<Long> shorter = voList.size() < runResult.size() ? voList
-                : runResult;
-        Set<Long> longer = voList.size() >= runResult.size() ? voList
-                : runResult;
+        Set<Long> shorter = voList.size() < runResult.size() ? voList : runResult;
+        Set<Long> longer = voList.size() >= runResult.size() ? voList : runResult;
         for (Long aLong : shorter) {
             if (longer.contains(aLong)) {
                 result.add(aLong);
@@ -122,8 +115,8 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
         return dm;
     }
 
-    private org.apache.lucene.search.Query getLuceneQueryForFields(
-            String searchString, String... fieldNames) throws ParseException {
+    private org.apache.lucene.search.Query getLuceneQueryForFields(String searchString, String... fieldNames)
+            throws ParseException {
         BooleanQuery bq = new BooleanQuery();
         if (isPhraseQuery(searchString)) {
             getPhraseQuery(searchString, fieldNames, bq);
@@ -135,21 +128,19 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
 
     private boolean isPhraseQuery(String searchString) {
         // Uncomment if you want to phrase querying
-        // return searchString.startsWith("\"") && searchString.endsWith("\"");
+//        return searchString.startsWith("\"") && searchString.endsWith("\"");
         return false;
     }
 
-    private void getTermQuery(String searchString, String[] fieldNames,
-            BooleanQuery bq) {
+    private void getTermQuery(String searchString, String[] fieldNames, BooleanQuery bq) {
         String[] split = searchString.replaceAll("\"", "").split(" ");
         TermQuery wq;
         int counter = 0;
         BooleanQuery internal;
         for (String singleString : split) {
             internal = new BooleanQuery();
-            while (counter < fieldNames.length) {
-                wq = new TermQuery(new Term(fieldNames[counter++],
-                        QueryParser.escape(singleString)));
+            while(counter < fieldNames.length) {
+                wq = new TermQuery(new Term(fieldNames[counter++], QueryParser.escape(singleString)));
                 internal.add(wq, Occur.SHOULD);
             }
             bq.add(internal, Occur.SHOULD);
@@ -157,28 +148,25 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
         }
     }
 
-    private void getPhraseQuery(String searchString, String[] fieldNames,
-            BooleanQuery bq) {
+    private void getPhraseQuery(String searchString, String[] fieldNames, BooleanQuery bq) {
         String[] split = searchString.replaceAll("\"", "").split(" ");
         PhraseQuery phraseQuery;
         int counter = 0;
         for (String singleString : split) {
             phraseQuery = new PhraseQuery();
-            while (counter < fieldNames.length) {
-                phraseQuery.add(new Term(fieldNames[counter++], QueryParser
-                        .escape(singleString)));
+            while(counter < fieldNames.length) {
+                phraseQuery.add(new Term(fieldNames[counter++], QueryParser.escape(singleString)));
             }
             bq.add(phraseQuery, Occur.MUST);
             counter = 0;
         }
     }
 
-    private Set<Long> searchSubscriptionViaLucene(
-            org.apache.lucene.search.Query query, FullTextSession fts)
+    private Set<Long> searchSubscriptionViaLucene(org.apache.lucene.search.Query query,
+                                                  FullTextSession fts)
             throws HibernateException {
         Set<Long> set = new LinkedHashSet<>();
-        FullTextQuery ftQuery = fts.createFullTextQuery(query,
-                Subscription.class);
+        FullTextQuery ftQuery = fts.createFullTextQuery(query, Subscription.class);
         ftQuery.setProjection("key");
         List<?> result = ftQuery.list();
         for (Object item : result) {
@@ -187,33 +175,30 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
         return set;
     }
 
-    private Set<Long> searchParametersViaLucene(
-            org.apache.lucene.search.Query query, FullTextSession fts)
+    private Set<Long> searchParametersViaLucene(org.apache.lucene.search.Query query,
+                                      FullTextSession fts)
             throws HibernateException {
         Set<Long> set = new LinkedHashSet<>();
         FullTextQuery ftQuery = fts.createFullTextQuery(query, Parameter.class);
         List<Parameter> result = ftQuery.list();
         for (Parameter item : result) {
-            set.add(item.getParameterSet().getProduct().getOwningSubscription()
-                    .getKey());
+            set.add(item.getParameterSet().getProduct().getOwningSubscription().getKey());
         }
         return set;
     }
 
     private Set<Long> searchUdasViaLucene(org.apache.lucene.search.Query query,
-            FullTextSession fts) throws HibernateException {
+                                      FullTextSession fts)
+            throws HibernateException {
         Set<Long> set = new LinkedHashSet<>();
         FullTextQuery ftQuery = fts.createFullTextQuery(query, Uda.class);
         List<Uda> result = ftQuery.list();
         SubscriptionDao subscriptionDao = getSubscriptionDao();
         for (Uda item : result) {
-            if (UdaTargetType.CUSTOMER.equals(item.getUdaDefinition()
-                    .getTargetType())) {
-                // Uda for organization, so get all subscription for
-                // organization
+            if(UdaTargetType.CUSTOMER.equals(item.getUdaDefinition().getTargetType())) {
+                //Uda for organization, so get all subscription for organization
                 List<Object[]> subs = subscriptionDao.getSubscriptionIdsForOrg(
-                        dm.getCurrentUser(), getStates(), item
-                                .getUdaDefinition().getOrganizationKey());
+                        dm.getCurrentUser(), getStates());
                 for (Object[] subColumns : subs) {
                     set.add(((BigInteger) subColumns[0]).longValue());
                 }
@@ -237,15 +222,14 @@ public class SubscriptionSearchServiceBean implements SubscriptionSearchService 
     }
 
     private String[] getSearchFieldsForSubscription() {
-        return new String[] { "dataContainer.purchaseOrderNumber",
-                "dataContainer.subscriptionId" };
+        return new String[]{"dataContainer.purchaseOrderNumber", "dataContainer.subscriptionId"};
     }
 
     private String[] getSearchFieldsForUda() {
-        return new String[] { "dataContainer.udaValue" };
+        return new String[]{"dataContainer.udaValue"};
     }
 
     private String[] getSearchFieldsForParameter() {
-        return new String[] { "dataContainer.value" };
+        return new String[]{"dataContainer.value"};
     }
 }
