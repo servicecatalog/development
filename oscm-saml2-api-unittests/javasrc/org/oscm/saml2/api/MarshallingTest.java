e3c2b87a5ba8f8b2c2497ee40cb9ceeb1be9d89b
grubskim <michal.grubski@ts.fujitsu.com>
2016-06-06 12:26:31 +0200

    Issue #21 Added new unit tests for Marshalling class

--- oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/MarshallingTest.java
+++ oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/MarshallingTest.java
@@ -9,18 +9,22 @@
 package org.oscm.saml2.api;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.Document;
 
 import org.oscm.calendar.GregorianCalendars;
 import org.oscm.converter.XMLConverter;
 import org.oscm.saml2.api.model.assertion.NameIDType;
 import org.oscm.saml2.api.model.protocol.AuthnRequestType;
+import org.oscm.saml2.api.model.protocol.LogoutRequestType;
+import org.oscm.saml2.api.model.xmldsig.SignatureType;
+
+import org.w3c.dom.Document;
 
 /**
  * @author roderus
@@ -29,12 +33,14 @@ import org.oscm.saml2.api.model.protocol.AuthnRequestType;
 public class MarshallingTest {
 
     private Marshalling<AuthnRequestType> marshaller;
+    private Marshalling<LogoutRequestType> logoutMarshaller;
     private org.oscm.saml2.api.model.protocol.ObjectFactory protocolObjFactory;
     private org.oscm.saml2.api.model.assertion.ObjectFactory assertionObjFactory;
 
     @Before
     public void setup() {
         marshaller = new Marshalling<AuthnRequestType>();
+        logoutMarshaller = new Marshalling<>();
         protocolObjFactory = new org.oscm.saml2.api.model.protocol.ObjectFactory();
         assertionObjFactory = new org.oscm.saml2.api.model.assertion.ObjectFactory();
     }
@@ -72,6 +78,42 @@ public class MarshallingTest {
         assertEquals("0", ACSResult);
     }
 
+    @Test
+    public void unmarshallElement() throws Exception {
+        //given
+        XMLGregorianCalendar gregCalendar = GregorianCalendars
+                .newXMLGregorianCalendarSystemTime();
+        JAXBElement<AuthnRequestType> authnRequestJAXB = createAuthnRequest(gregCalendar);
+        authnRequestJAXB.getValue().getIssuer().setValue("someIssuer");
+        final SignatureType signature = new SignatureType();
+        signature.setId("someSigID");
+        authnRequestJAXB.getValue().setSignature(signature);
+        authnRequestJAXB.getValue().setVersion("someVersion");
+        Document authnRequestDoc = marshaller.marshallElement(authnRequestJAXB);
+        // when
+        JAXBElement<AuthnRequestType> unmarshalled = marshaller.unmarshallDocument(authnRequestDoc.getDocumentElement(), AuthnRequestType.class);
+        //then
+        assertTrue(unmarshalled.getValue() instanceof AuthnRequestType);
+        assertTrue(unmarshalled.getValue().getIssuer().getValue().equals("someIssuer"));
+        assertTrue(unmarshalled.getValue().getSignature().getId().equals("someSigID"));
+        assertTrue(unmarshalled.getValue().getVersion().equals("someVersion"));
+    }
+    @Test
+    public void unmarshallElement_logout() throws Exception {
+        //given
+        XMLGregorianCalendar gregCalendar = GregorianCalendars
+                .newXMLGregorianCalendarSystemTime();
+        JAXBElement<LogoutRequestType> logoutRequestJAXB = createLogoutRequest(gregCalendar);
+        Document authnRequestDoc = logoutMarshaller.marshallElement(logoutRequestJAXB);
+        // when
+        JAXBElement<LogoutRequestType> unmarshalled = logoutMarshaller.unmarshallDocument(authnRequestDoc.getDocumentElement(), LogoutRequestType.class);
+        //then
+        assertTrue(unmarshalled.getValue() instanceof LogoutRequestType);
+        assertTrue(unmarshalled.getValue().getIssuer().getValue().equals("someIssuer"));
+        assertTrue(unmarshalled.getValue().getSignature().getId().equals("someSigID"));
+        assertTrue(unmarshalled.getValue().getVersion().equals("someVersion"));
+    }
+
     private JAXBElement<AuthnRequestType> createAuthnRequest(
             XMLGregorianCalendar gregCalendar) {
         NameIDType issuer = assertionObjFactory.createNameIDType();
@@ -86,5 +128,23 @@ public class MarshallingTest {
         authnRequest.setAssertionConsumerServiceIndex(Integer.valueOf(0));
 
         return protocolObjFactory.createAuthnRequest(authnRequest);
+    }
+
+    private JAXBElement<LogoutRequestType> createLogoutRequest(
+            XMLGregorianCalendar gregCalendar) {
+        NameIDType issuer = assertionObjFactory.createNameIDType();
+        final SignatureType signature = new SignatureType();
+        LogoutRequestType logoutRequest = protocolObjFactory
+                .createLogoutRequestType();
+
+        issuer.setValue("someIssuer");
+        logoutRequest.setIssuer(issuer);
+        logoutRequest.setID("someID");
+        logoutRequest.setVersion("someVersion");
+        logoutRequest.setIssueInstant(gregCalendar);
+        signature.setId("someSigID");
+        logoutRequest.setSignature(signature);
+
+        return protocolObjFactory.createLogoutRequest(logoutRequest);
     }
 }
