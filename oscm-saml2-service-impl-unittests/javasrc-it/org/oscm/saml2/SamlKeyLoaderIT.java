e2fff99cd2456f399f3a4dbc9343a76349cf5584
grubskim <michal.grubski@ts.fujitsu.com>
2016-06-24 14:32:39 +0200

    Issue #21 Added IT test

---
+++ oscm-saml2-service-impl-unittests/javasrc-it/org/oscm/saml2/SamlKeyLoaderIT.java
@@ -0,0 +1,86 @@
+package org.oscm.saml2;
+
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.oscm.internal.types.exception.SaaSApplicationException;
+
+/**
+ * Created by PLGrubskiM on 2016-06-24.
+ */
+public class SamlKeyLoaderIT {
+    private SamlKeyLoader samlKeyLoader;
+
+    private static final String KEYSTORE_PASSWORD = "changeit";
+    private static final String ALIAS = "s1as";
+
+    @Before
+    public void setUp() {
+        samlKeyLoader = new SamlKeyLoader();
+    }
+
+    @Test
+    public void loadPrivateKeyFromStore_OK() throws CertificateException,
+            SaaSApplicationException, UnrecoverableKeyException,
+            NoSuchAlgorithmException, IOException, KeyStoreException {
+        // when
+        samlKeyLoader.loadPrivateKeyFromStore(getKeystoreFilePath(),
+                KEYSTORE_PASSWORD, ALIAS);
+        // then
+    }
+
+    @Test(expected = IOException.class)
+    public void loadPrivateKeyFromStore_invalidFile()
+            throws CertificateException, SaaSApplicationException,
+            UnrecoverableKeyException, NoSuchAlgorithmException, IOException,
+            KeyStoreException {
+        // given
+        String path = "somePath";
+        // when
+        samlKeyLoader.loadPrivateKeyFromStore(path, KEYSTORE_PASSWORD, ALIAS);
+    }
+
+    @Test(expected = IOException.class)
+    public void loadPrivateKeyFromStore_invalidPassword()
+            throws CertificateException, SaaSApplicationException,
+            UnrecoverableKeyException, NoSuchAlgorithmException, IOException,
+            KeyStoreException {
+        // given
+        String password = "invalidPassword";
+        // when
+        samlKeyLoader.loadPrivateKeyFromStore(getKeystoreFilePath(), password,
+                ALIAS);
+        // then
+    }
+
+    @Test(expected = SaaSApplicationException.class)
+    public void loadPrivateKeyFromStore_invalidAlias()
+            throws CertificateException, SaaSApplicationException,
+            UnrecoverableKeyException, NoSuchAlgorithmException, IOException,
+            KeyStoreException {
+        // when
+        samlKeyLoader.loadPrivateKeyFromStore(getKeystoreFilePath(),
+                KEYSTORE_PASSWORD, "invalidAlias");
+    }
+
+    @Test // TODO
+    public void loadPrivateKeyFromStore_expired() throws CertificateException,
+            SaaSApplicationException, UnrecoverableKeyException,
+            NoSuchAlgorithmException, IOException, KeyStoreException {
+        // when
+        samlKeyLoader.loadPrivateKeyFromStore(getKeystoreFilePath(),
+                KEYSTORE_PASSWORD, ALIAS);
+    }
+
+    public String getKeystoreFilePath() {
+        return getClass().getClassLoader().getResource("").getPath()
+                + "/../javares/keystore.jks";
+    }
+
+}
