2ee1118b93c7d9a4a6a71f1a2c679bcef4bca311
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2016-06-27 09:23:31 +0200

    Fixes #21. Added unittest for logout generator bean.

--- oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/LogoutRequestGeneratorTest.java
+++ oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/LogoutRequestGeneratorTest.java
@@ -11,23 +11,36 @@ package org.oscm.saml2.api;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.oscm.internal.intf.SamlService;
 import org.oscm.internal.types.exception.SaaSApplicationException;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Added by @chojnackid
  */
+@RunWith(MockitoJUnitRunner.class)
 public class LogoutRequestGeneratorTest {
 
     LogoutRequestGenerator classUnderStress;
 
+    @Mock
+    private SamlService mock;
+
     @Before
-    public void setup() {
+    public void setup() throws SaaSApplicationException {
+        doReturn("").when(mock).generateLogoutRequest("", "");
         classUnderStress = new LogoutRequestGenerator();
+        classUnderStress.setSamlService(mock);
     }
 
     @Test
     public void generateLogoutRequestTest() throws SaaSApplicationException {
-        String URL = "ignore";//classUnderStress.generateLogoutRequest("", "");
+        String URL = classUnderStress.generateLogoutRequest("", "");
         Assert.assertNotNull(URL);
+        verify(mock, times(1)).generateLogoutRequest("", "");
     }
 }
