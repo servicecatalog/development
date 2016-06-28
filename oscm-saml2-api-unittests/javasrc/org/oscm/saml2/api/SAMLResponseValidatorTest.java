a7a4fc4aa1436d9a7175dc6bf6231d02cf88f349
grubskim <michal.grubski@ts.fujitsu.com>
2016-06-28 11:12:58 +0200

    Issue #21 Fixed test and removed unused files

--- oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/SAMLResponseValidatorTest.java
+++ oscm-saml2-api-unittests/javasrc/org/oscm/saml2/api/SAMLResponseValidatorTest.java
@@ -11,18 +11,10 @@ package org.oscm.saml2.api;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.FileInputStream;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.oscm.converter.XMLConverter;
 import org.oscm.internal.types.exception.SAML2StatusCodeInvalidException;
-import org.oscm.stream.Streams;
-
-import org.w3c.dom.Document;
 
 /**
  * Tests the validating the SAML LogoutResponse status code.
@@ -32,9 +24,12 @@ import org.w3c.dom.Document;
  */
 public class SAMLResponseValidatorTest {
 
-    private final String FILE_UNSIGNED_LOGOUT_RESPONSE = "javares/openamUnsignedLogoutResponse.xml";
-    private final String FILE_UNSIGNED_LOGOUT_RESPONSE_ERROR_STATUS = "javares/openamUnsignedLogoutResponse_ErrorCode.xml";
-    private final String FILE_UNSIGNED_LOGOUT_RESPONSE_INVALID_STATUS = "javares/openamUnsignedLogoutResponse_InvalidStatus.xml";
+    // Deflated and Base64 encoded strings containing SAML LogoutResponse.
+    // This is to simulate the identity provider behavior (we receive responses
+    // in this format)
+    private final String FILE_UNSIGNED_LOGOUT_RESPONSE = "rVNbS8MwFH4X/A+j72uT9JaGtTAcyGAXcUPFF0nTdKu0SelJwZ9vVx12YPcghjydJN/lfCcz4FVZs5U+6NY8Sqi1Ajn5qEoFrD+KrbZRTHMogCleSWBGsN18vWLERqxutNFCl9btzeTXtVzEFlDuysB1iRCRCLgXpCQLUkojlObSDZAXelnu+9QfRXmSDRRaxVbHOU4F0MqlAsOV6W4iHExRt/EeUUYow+7r6NOFBFMobnqOozE1c5x7RB5WL5vN9plEKAjtAyJ23r4XBlq71IKXjGKKHA2imta6Mbx0xqWpc2v3OraWi7csjDKcoxRTEgrKeUgDFIaSyky6qZ9hl3QtyjxiJV+Qs1MWrHfYDNK5Hg4HkM3Jk5V8e+p1HzUYRlGnfVtLNV9PsWsjG82cAceQtmY7w00Lf5iK5KchF1B3OvvPIetHhJetvI4DPTfbtUJIgAGac7brDEV2xXPh8nsknw==";
+    private final String FILE_UNSIGNED_LOGOUT_RESPONSE_ERROR_STATUS = "rVNba4MwFH4f7D8U36u5eImhCmWFUehltGUbexlRY+vQxHki7OfPupVZWPswFvJ0knyX851MQFRlzRd6r1uzkVBrBXL0UZUKeH8UWW2juBZQAFeiksBNyrfT5YITG/G60UanurRub0a/rvkssoAJKn1KSZqGqS9cPyGZnzAWoiSX1Edu4Ga55zHvIsqjbKDQKrI6zstUAK2cKzBCme4mwv4YdRvvEOOEcUxfLj6dSTCFEqbnOBhTc8e5R+Rh8bxarZ9IiPzA3iNi5+1bYaC1S52KkjPMkKMhrca1bowoncvS1Km1Ox1Z89lrFoQZzlGCGQlSJkTAfBQEkslM0sTLMCVdizKXWPEX5OSYBe8dNoN0rocjAGRz9GTF35563QcNhjPUaV/XUk2XY0xtZKOJM+AY0tZ8a4Rp4Q9TEf805AzqTmf/OWT9iIiylddxoOfmG/nednnLZoDnnAw7Q5ld8VQ4/yDxJw==";
+    private final String FILE_UNSIGNED_LOGOUT_RESPONSE_INVALID_STATUS = "rVNbS8MwFH4X/A+j72vT9JaGtTAsyGAXcUPFF0mTdKu0SelJQf+9tTrswO1BDHk6Sb7L+U5mwOqqoUu91525l9BoBXLyVlcK6HCUWF2rqGZQAlWslkANp9v5akmxjWjTaqO5rqzrq8mva5ElFhDmydDzMOcxD5kf5liEOSExygvphciPfFEEAQnOojzIFkqtEqvnPE8F0MmFAsOU6W8iN5yifrs7RCgm1PWezz7NJJhSMTNwHIxpqOPcIny3fFqvN484RmFk7xG2i+61NNDZleasosQlyNHA62mjW8Mq57w0dWztTifWInsRUSzcAuUuwREnjEUkRFEkiRTSywPherhvkfCxlX5Bzj6zoIPDdpTO5XAYgGw/PVnpt6dB90GDoQT12jeNVPPV1PVsZKOZM+IY0zZ0a5jp4A9Tkf405ATqRov/HLJhRFjVycs4MHDTrKvr9xGWczTrjCX2xWPh9HOkHw==";
     private SAMLLogoutResponseValidator samlLogoutResponseValidator;
 
     @Before
@@ -42,62 +37,32 @@ public class SAMLResponseValidatorTest {
         samlLogoutResponseValidator = new SAMLLogoutResponseValidator();
     }
 
-    private Document loadDocument(String file) throws Exception {
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(file);
-            Document document = XMLConverter.convertToDocument(inputStream);
-            inputStream.close();
-            return document;
-        } finally {
-            Streams.close(inputStream);
-        }
-    }
-
-    private String encode(String s) {
-        return Base64.encodeBase64String(StringUtils.getBytesUtf8(s));
-    }
-
-    private String getEncodedIdpResponse(String unsignedResponse)
-            throws Exception {
-        Document document = loadDocument(unsignedResponse);
-        String idpResponse = XMLConverter.convertToString(document, true);
-        String encodedIdpResponse = encode(idpResponse);
-        return encodedIdpResponse;
-    }
-
     @Test
     public void responseStatusCodeSuccessful_noError() throws Exception {
-        // given
-        String response = getEncodedIdpResponse(FILE_UNSIGNED_LOGOUT_RESPONSE);
         // when
-        final boolean successful = samlLogoutResponseValidator.responseStatusCodeSuccessful(response);
+        final boolean successful = samlLogoutResponseValidator
+                .responseStatusCodeSuccessful(FILE_UNSIGNED_LOGOUT_RESPONSE);
         // then
         assertTrue(successful);
     }
 
     @Test(expected = SAML2StatusCodeInvalidException.class)
     public void responseStatusCodeSuccessful_invalidStatus() throws Exception {
-
-        // given
-        String response = getEncodedIdpResponse(FILE_UNSIGNED_LOGOUT_RESPONSE_INVALID_STATUS);
         // when
-        final boolean successful = samlLogoutResponseValidator.responseStatusCodeSuccessful(response);
+        final boolean successful = samlLogoutResponseValidator
+                .responseStatusCodeSuccessful(
+                        FILE_UNSIGNED_LOGOUT_RESPONSE_INVALID_STATUS);
         // then
         assertFalse(successful);
     }
 
     @Test
     public void responseStatusCodeSuccessful_hasError() throws Exception {
-
-        // given
-        String response = getEncodedIdpResponse(FILE_UNSIGNED_LOGOUT_RESPONSE_ERROR_STATUS);
         // when
-        final boolean successful = samlLogoutResponseValidator.responseStatusCodeSuccessful(response);
+        final boolean successful = samlLogoutResponseValidator
+                .responseStatusCodeSuccessful(
+                        FILE_UNSIGNED_LOGOUT_RESPONSE_ERROR_STATUS);
         // then
         assertFalse(successful);
     }
-
-
-
 }
