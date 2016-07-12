1a5b5eb3ff210dd221153308206694803099381c
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2016-07-12 16:55:45 +0200

    Fixes #167. Added possibility to update VCenter entry

--- oscm-app-vmware-setup/javasrc/org/oscm/app/vmware/db/importer/VCenterImport.java
+++ oscm-app-vmware-setup/javasrc/org/oscm/app/vmware/db/importer/VCenterImport.java
@@ -11,6 +11,8 @@ package org.oscm.app.vmware.db.importer;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -51,9 +53,17 @@ public class VCenterImport extends GenericImport {
                 String userid = line.get(VCenterCSV.COL_USER_ID);
                 String password = line.get(VCenterCSV.COL_PASSWORD);
                 String tkey = line.get(VCenterCSV.COL_TKEY);
+                boolean rowExists = false;
+                String oldVcId = null;
+                
                 try {
-                    addTableRow(conn, vcenterId, vcenterName, url, userid,
-                            password, tkey);
+                    rowExists = rowExists(conn, tkey);
+                    if(rowExists){
+                        oldVcId = getVCenterId(conn, tkey);
+                        updateTableRow(conn, vcenterId, vcenterName, url, userid, password, tkey);
+                    } else{
+                        addTableRow(conn, vcenterId, vcenterName, url, userid, password, tkey);
+                    }
                 } catch (Exception e) {
                     logger.error("failed to add row:  " + tkey + " " + vcenterId
                             + " " + vcenterName);
@@ -62,7 +72,13 @@ public class VCenterImport extends GenericImport {
                     return;
                 }
                 try {
-                    createSequence(conn, vcenterId);
+                    if (rowExists) {
+                        if (!vcenterId.equals(oldVcId)) {
+                            updateSequence(conn, oldVcId, vcenterId);
+                        }
+                    } else {
+                        createSequence(conn, vcenterId);
+                    }
                 } catch (Exception e) {
                     logger.error("failed to create sequence for   " + tkey + " "
                             + vcenterId + " " + vcenterName);
@@ -95,7 +111,8 @@ public class VCenterImport extends GenericImport {
             }
         }
     }
-
+    
+    
     private void createSequence(Connection con, String vcenterId)
             throws Exception {
         String query = "create sequence vcenter_" + vcenterId.trim()
@@ -107,7 +124,7 @@ public class VCenterImport extends GenericImport {
 
     private void addTableRow(Connection con, String vcId, String vcName,
             String url, String userid, String password, String tkey)
-            throws Exception {
+                    throws Exception {
 
         String query = "insert into vcenter (TKEY, NAME, IDENTIFIER, URL, USERID, PASSWORD) values(?, ?, ?, ?, ?, ?)";
 
@@ -121,5 +138,66 @@ public class VCenterImport extends GenericImport {
             stmt.execute();
         }
     }
+    
+    private void updateTableRow(Connection con, String vcId, String vcName,
+            String url, String userid, String password, String tkey)
+                    throws Exception {
 
+        String query = "UPDATE vcenter set NAME=?, IDENTIFIER=?, URL=?, USERID=?, PASSWORD=? WHERE TKEY=?";
+
+        try (PreparedStatement stmt = con.prepareStatement(query);) {
+            stmt.setString(1, vcName);
+            stmt.setString(2, vcId);
+            stmt.setString(3, url);
+            stmt.setString(4, userid);
+            stmt.setString(5, password);
+            stmt.setInt(6, Integer.parseInt(tkey));
+            stmt.execute();
+        }
+    }
+    
+    private void updateSequence(Connection con, String oldVcId, String newVcId)
+            throws Exception {
+        String query = "alter sequence vcenter_" + oldVcId.trim()
+                + "_seq rename to vcenter_" + newVcId.trim() + "_seq";
+        try (PreparedStatement stmt = con.prepareStatement(query);) {
+            stmt.execute();
+        }
+    }
+    
+    private boolean rowExists(Connection con, String tkey) throws Exception {
+        String query = "select count(*) as count FROM vcenter WHERE TKEY=?";
+
+        try (PreparedStatement stmt = con.prepareStatement(query);) {
+            stmt.setInt(1, Integer.parseInt(tkey));
+            ResultSet resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt("count");
+                if (count > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+    
+    private String getVCenterId(Connection con, String tkey) {
+
+        String query = "SELECT identifier FROM vcenter WHERE tkey = ?";
+        try (PreparedStatement stmt = con.prepareStatement(query);) {
+            stmt.setInt(1, Integer.parseInt(tkey));
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to retrieve vcenter " + tkey,
+                    e);
+        }
+        return null;
+    }
 }
