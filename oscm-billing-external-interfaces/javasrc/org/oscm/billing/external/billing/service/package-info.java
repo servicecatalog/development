a2b7984e4b623890bf074412cf99cdfc1bdb749b
SuessI <inge.suess@est.fujitsu.com>
2016-07-14 09:57:13 +0200

    Review, correct, and complete the javadoc of the billing plug-in API.

---
+++ oscm-billing-external-interfaces/javasrc/org/oscm/billing/external/billing/service/package-info.java
@@ -0,0 +1,9 @@
+/* 
+ *  Copyright FUJITSU LIMITED 2016 
+ */
+/**
+ * Provides the interface a billing adapter must implement for 
+ * establishing and testing the connection with the external 
+ * billing system. 
+ */
+package org.oscm.billing.external.billing.service;
