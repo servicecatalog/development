81e2c939dcf8f518df91afd897994a1d1af66617
miethaner <robert.miethaner@est.fujitsu.com>
2016-10-05 11:12:07 +0200

    add new error messages for stack name pattern

--- oscm-app-openstack/javasrc/org/oscm/app/openstack/controller/ProvisioningValidator.java
+++ oscm-app-openstack/javasrc/org/oscm/app/openstack/controller/ProvisioningValidator.java
@@ -10,6 +10,7 @@ package org.oscm.app.openstack.controller;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.oscm.app.openstack.i18n.Messages;
 import org.oscm.app.v1_0.exceptions.APPlatformException;
@@ -36,11 +37,20 @@ public abstract class ProvisioningValidator {
 
         String stackNamePattern = paramHandler.getStackNamePattern();
         if (!isNullOrEmpty(stackNamePattern)) {
-            p = Pattern.compile(stackNamePattern);
+
+            try {
+                p = Pattern.compile(stackNamePattern);
+            } catch (PatternSyntaxException e) {
+                logger.error("Compile error on stack name pattern: [" + stackNamePattern + "]");
+                throw new APPlatformException(
+                        Messages.getAll("error_invalid_pattern", new Object[] { stackNamePattern }));
+            }
+
             m = p.matcher(stackName);
             if (!m.matches()) {
                 logger.error("Validation error on stack name: [" + stackName + "/" + stackNamePattern + "]");
-                throw new APPlatformException(Messages.getAll("error_invalid_name", new Object[] { stackName }));
+                throw new APPlatformException(
+                        Messages.getAll("error_name_match", new Object[] { stackName, stackNamePattern }));
             }
         }
     }
