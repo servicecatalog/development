252cf9f70d73d02502ddc0f69a5b3c432740fee9
Dawid Chojnacki <Dawid.Chojnacki@ts.fujitsu.com>
2016-10-07 10:23:37 +0200

    Fixing it test.

--- oscm-security-unittests/javasrc-it/org/oscm/security/ADMRealmImplIT.java
+++ oscm-security-unittests/javasrc-it/org/oscm/security/ADMRealmImplIT.java
@@ -15,9 +15,7 @@ package org.oscm.security;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.matches;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -68,7 +66,8 @@ public class ADMRealmImplIT extends EJBTestBase {
     private static final String WS_PASSWORD = "WSDummyPassword";
     private static final String REQUEST_ID = "ID_0123456789012345678901234567890123456789";
     private static final String SAML_RESPONSE = "SAMLResponse";
-    private static final String UI_PASSWORD = "UI" + REQUEST_ID + SAML_RESPONSE;
+    private static final String TENANT_ID = "11111111";
+    private static final String UI_PASSWORD = "UI" + REQUEST_ID + TENANT_ID + SAML_RESPONSE;
     public static final String tenantID = "";
 
     @Override
@@ -112,7 +111,7 @@ public class ADMRealmImplIT extends EJBTestBase {
         acs = mock(AssertionConsumerService.class);
         doReturn(acs).when(realm).getAssertionConsumerService(
                 any(AuthenticationModeQuery.class));
-        doNothing().when(acs).validateResponse(anyString(), anyString(), tenantID);
+        doNothing().when(acs).validateResponse(anyString(), anyString(), eq(tenantID));
     }
 
     @Test
@@ -282,7 +281,7 @@ public class ADMRealmImplIT extends EJBTestBase {
                 mock(AuthenticationModeQuery.class));
 
         // then
-        verify(acs, times(1)).validateResponse(SAML_RESPONSE, REQUEST_ID, tenantID);
+        verify(acs, times(1)).validateResponse(SAML_RESPONSE, REQUEST_ID, TENANT_ID);
     }
 
     @Test
