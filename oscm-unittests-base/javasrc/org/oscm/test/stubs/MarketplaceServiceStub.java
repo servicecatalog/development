ae4533ca395fcd553e59c69d45c86f784ceaa888
Ronny Weiser <ronny.weiser@est.fujitsu.com>
2016-11-03 13:14:38 +0100

    helper method to retrieve marketplace id for key

--- oscm-unittests-base/javasrc/org/oscm/test/stubs/MarketplaceServiceStub.java
+++ oscm-unittests-base/javasrc/org/oscm/test/stubs/MarketplaceServiceStub.java
@@ -241,4 +241,10 @@ public class MarketplaceServiceStub implements MarketplaceService {
         throw new UnsupportedOperationException();
     }
 
+    @Override
+    public String getMarketplaceIdForKey(Long key)
+            throws ObjectNotFoundException {
+        throw new UnsupportedOperationException();
+    }
+
 }
