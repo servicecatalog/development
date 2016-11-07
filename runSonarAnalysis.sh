9e06924bd70b03ab0581effbf4727b2e45587b7c
Dawid <dawid.chojnacki@ts.fujitsu.com>
2016-11-07 12:59:41 +0100

    Update runSonarAnalysis.sh

--- runSonarAnalysis.sh
+++ runSonarAnalysis.sh
@@ -29,10 +29,8 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	#
 	# Analysis is done only on master so that build of branches don't push analyses to the same project and therefore "pollute" the results
 	echo "Starting analysis by SonarQube..."
-	sonar-runner \
-	#sonar-scanner \
-		-Dsonar.host.url=$SONAR_HOST_URL \
-		-Dsonar.login=$SONAR_TOKEN
+	sonar-runner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+	#sonar-scanner
 
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
 	# => This will analyse the PR and display found issues as comments in the PR, but it won't push results to the SonarQube server
