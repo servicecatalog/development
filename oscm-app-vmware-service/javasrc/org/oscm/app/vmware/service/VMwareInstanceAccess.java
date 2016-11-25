008bc169841ba803002989fdc23beb1c09fe01ae
Suzana Stavreva <suzana.stavreva@est.fujitsu.com>
2016-11-25 09:22:00 +0100

    Add implementation to satisfy CDI

---
+++ oscm-app-vmware-service/javasrc/org/oscm/app/vmware/service/VMwareInstanceAccess.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2016                                           
+ *                                                                                                                                 
+ *  Creation Date: 2016/11/11                                                      
+ *                                                                              
+ *******************************************************************************/
+
+package org.oscm.app.vmware.service;
+
+import java.util.List;
+
+import org.oscm.app.common.intf.InstanceAccess;
+import org.oscm.app.common.intf.ServerInformation;
+import org.oscm.app.v2_0.exceptions.APPlatformException;
+
+/**
+ * @author tateiwamext
+ * 
+ */
+public class VMwareInstanceAccess implements InstanceAccess {
+
+    private static final long serialVersionUID = 5685290474829749127L;
+
+    @Override
+    public List<? extends ServerInformation> getServerDetails(String instanceId)
+            throws APPlatformException {
+        return null;
+    }
+
+    @Override
+    public String getAccessInfo(String instanceId) throws APPlatformException {
+        return null;
+    }
+
+    @Override
+    public String getMessage(String locale, String key, Object... arguments) {
+        return null;
+    }
+
+}
