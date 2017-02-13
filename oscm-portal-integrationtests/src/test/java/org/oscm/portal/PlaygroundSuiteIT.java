920fc3c157f3ab01b149601f759da0e3269b5561
miethaner <robert.miethaner@est.fujitsu.com>
2017-02-13 11:00:22 +0100

    add selenium web test framework and basic portal tests

---
+++ oscm-portal-integrationtests/src/test/java/org/oscm/portal/PlaygroundSuiteIT.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2017                                           
+ *                                                                                                                                 
+ *  Creation Date: Feb 7, 2017                                                      
+ *                                                                              
+ *******************************************************************************/
+
+package org.oscm.portal;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test suite for integration web tests for the OSCM portal
+ * 
+ * @author miethaner
+ */
+@RunWith(Suite.class)
+
+@SuiteClasses({ PortalOrganizationWT.class, PortalMarketplaceWT.class,
+        PortalTechServiceWT.class, PortalMarketServiceWT.class })
+public class PlaygroundSuiteIT {
+
+    public static String supplierOrgId;
+    public static String customerOrgId;
+}
