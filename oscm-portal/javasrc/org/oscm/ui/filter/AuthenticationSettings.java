48176c0f7ca0c8c670dfb13a5978de571ca10313
grubskim <michal.grubski@ts.fujitsu.com>
2017-02-15 15:29:09 +0100

    Fix for failing tests

--- oscm-portal/javasrc/org/oscm/ui/filter/AuthenticationSettings.java
+++ oscm-portal/javasrc/org/oscm/ui/filter/AuthenticationSettings.java
@@ -124,7 +124,7 @@ public class AuthenticationSettings {
     }
 
     private void validateTenant(VOTenant tenant) throws WrongTenantConfigurationException {
-        if (getConfigurationSetting(cfgService, ConfigurationKey.AUTH_MODE).equals("INTERNAL")) {
+        if (isInternal()) {
             return;
         }
         for (IdpSettingType idpSettingType : getMandatorySSOSettingKeys()) {
