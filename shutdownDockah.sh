5dc52670137343adceeedbb67b25948e5b7171b7
digitalfishpond <denis.poisson@ts.fujitsu.com>
2017-03-09 09:00:42 +0100

    added webtest.properties for jenkins

--- shutdownDockah.sh
+++ shutdownDockah.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker ps -all | awk '{if ($2 == "servicecatalog/oscmglassfish4") print $1;}'
 docker ps -all | awk '{if ($2 == "servicecatalog/oscmglassfish4") print $1;}' | xargs docker stop
