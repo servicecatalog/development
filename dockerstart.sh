6b46784171892e23d84dcab62a9c0637202471b6
digitalfishpond <denis.poisson@ts.fujitsu.com>
2017-03-29 12:49:28 +0200

    changed port back to 5003

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #move image name to jenkins job variable
-docker login --username=tech-oscm --email=tech-oscm@est.fujitsu.com artifactory.intern.est.fujitsu.com:5002 && sudo docker pull artifactory.intern.est.fujitsu.com:5002/ctmg:latest
+docker login --username=tech-oscm --email=tech-oscm@est.fujitsu.com artifactory.intern.est.fujitsu.com:5003 && sudo docker pull artifactory.intern.est.fujitsu.com:5003/ctmg:latest
 docker run -d -i -p 8080:8080 artifactory.intern.est.fujitsu.com:5002/ctmg:latest
 sleep 150
