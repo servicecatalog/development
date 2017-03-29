fc2be2b7a071de934c56043ef16858a669b48085
digitalfishpond <denis.poisson@ts.fujitsu.com>
2017-03-29 11:46:36 +0200

    pull from artifactory 5002 (docker-dev-local2) instead of dockerhub

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#move image name to jenkins job variable 
-docker pull servicecatalog/oscm:latest
-docker run -d -i -p 8080:8080 servicecatalog/oscm:latest
+#move image name to jenkins job variable
+sudo docker login --username=tech-oscm --email=tech-oscm@est.fujitsu.com artifactory.intern.est.fujitsu.com:5002 && sudo docker pull artifactory.intern.est.fujitsu.com:5002/ctmg:latest
+docker run -d -i -p 8080:8080 artifactory.intern.est.fujitsu.com:5002/ctmg:latest
 sleep 150
