f851ce63cb3906615a001e5663f9b56a74a1a302
miethaner <robert.miethaner@est.fujitsu.com>
2017-04-13 10:47:56 +0200

    remove timezone from template table

--- oscm-app/resources/sql/upd_postgresql_02_06_08.sql
+++ oscm-app/resources/sql/upd_postgresql_02_06_08.sql
@@ -3,7 +3,7 @@ CREATE TABLE "templatefile"
   "tkey" BIGINT NOT NULL, 
   "filename" character varying(255) NOT NULL,
   "content" OID NOT NULL,
-  "lastchange" timestamp with time zone NOT NULL,
+  "lastchange" timestamp NOT NULL,
   "controllerid" character varying(255),
   CONSTRAINT "templatefile_pk" PRIMARY KEY ("tkey"),
   CONSTRAINT "templatefile_uc" UNIQUE ("filename", "controllerid")
