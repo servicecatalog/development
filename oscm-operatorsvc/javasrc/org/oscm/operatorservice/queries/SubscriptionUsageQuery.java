4657f6c17b2ebb2ded65e17351d962e0f37ca232
Dawid <dawid.chojnacki@ts.fujitsu.com>
2017-05-31 11:49:47 +0200

    Added parameter limitation

--- oscm-operatorsvc/javasrc/org/oscm/operatorservice/queries/SubscriptionUsageQuery.java
+++ oscm-operatorsvc/javasrc/org/oscm/operatorservice/queries/SubscriptionUsageQuery.java
@@ -49,7 +49,9 @@ public class SubscriptionUsageQuery {
                 "                parameterdefinition.technicalproduct_tkey=product.technicalproduct_tkey AND\n" +
                 "                subscription.organizationkey = organization.tkey AND\n" +
                 "                product.technicalproduct_tkey = technicalproduct.tkey AND\n" +
-                "                technicalproduct.organizationkey = supplier.tkey\n" +
+                "                technicalproduct.organizationkey = supplier.tkey AND\n" +
+                "                parameter.value ~ '^\d{1,}$'\n" +
+                "                parameter.value::int > 0\n" +
                 "                ORDER BY organization.organizationid;\n";
 
 
