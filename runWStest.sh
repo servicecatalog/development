af5d33db9d73dc7e335f2b4d5eafcf78e45db810
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2017-06-20 10:34:06 +0200

    Issue #31 - Modified path to ant.

--- runWStest.sh
+++ runWStest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -i default /opt/apache-ant-1.9.7/bin/ant -f /opt/development/oscm-build/cruisecontrol.xml -lib /opt/development/libraries/eclipse-ecj/javalib/ _webserviceTests -Dhostname=default
+docker exec -i default ant -f /opt/development/oscm-build/cruisecontrol.xml -lib /opt/development/libraries/eclipse-ecj/javalib/ _webserviceTests -Dhostname=default
