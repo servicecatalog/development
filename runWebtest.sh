594cb1ab94a7cd7e7aad788440559b91ca4e0557
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2017-06-20 13:42:36 +0200

    Added webtests runner

---
+++ runWebtest.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker exec -i default /opt/apache-ant/bin/ant -f /opt/development/oscm-build/cruisecontrol.xml -lib /opt/development/libraries/eclipse-ecj/javalib/ _webTests -Dhostname=default
