ca67861143959ed82ba265d5092fcbf29ed57436
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2017-06-20 07:51:21 +0200

    Issue #31 - Removing default container before running tests.

--- runWStest.sh
+++ runWStest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker rm default
+docker stop default && docker rm default && \
 docker exec default /opt/apache-ant/bin/ant -f /opt/development/oscm-build/cruisecontrol.xml -lib /opt/development/libraries/eclipse-ecj/javalib/ _webserviceTests -Dhostname=default
