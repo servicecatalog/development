76420f406931f033666651bc5342d06fa6886f04
Dawid Chojnacki <dawid.chojnacki@ts.fujitsu.com>
2017-06-20 08:54:02 +0200

    Issue #31 - Removing default container before running tests.

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #move image name to jenkins job variable
 docker login --username=tech-oscm --email=tech-oscm@est.fujitsu.com artifactory.intern.est.fujitsu.com:5003 && docker pull artifactory.intern.est.fujitsu.com:5003/ctmg:latest
+docker stop default && docker rm default
 docker run --add-host=oscm.org:127.0.0.1 --name=default -h default -d -i -p 8080:8080 -p 8081:8081 -p 8048:8048 -p 8037:8037 artifactory.intern.est.fujitsu.com:5003/ctmg:latest
 sleep 180
--- runWStest.sh
+++ runWStest.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker stop default && docker rm default && \
 docker exec default /opt/apache-ant/bin/ant -f /opt/development/oscm-build/cruisecontrol.xml -lib /opt/development/libraries/eclipse-ecj/javalib/ _webserviceTests -Dhostname=default
