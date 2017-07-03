0d4eceb6a5246f6734645901b96aeb54edb4c96f
stavreva <stavreva@eststavreva.intern.est.fujitsu.com>
2017-07-03 08:33:38 +0200

    Add sql for altering table

--- oscm-devruntime/javares/sql/upd_postgresql_02_09_13.sql
+++ oscm-devruntime/javares/sql/upd_postgresql_02_09_13.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE "subscription" ADD COLUMN "uuid" uuid;
--- ALTER TABLE "subscription" ADD COLUMN "eventpublished" boolean NOT NULL DEFAULT false;
--- ALTER TABLE "subscriptionhistory" ADD "eventpublished" boolean NOT NULL DEFAULT false;
+ALTER TABLE "subscription" ADD COLUMN "uuid" uuid;
+ALTER TABLE "subscription" ADD COLUMN "eventpublished" boolean NOT NULL DEFAULT false;
+ALTER TABLE "subscriptionhistory" ADD "eventpublished" boolean NOT NULL DEFAULT false;
 
 run:MigrateSubscriptions;
