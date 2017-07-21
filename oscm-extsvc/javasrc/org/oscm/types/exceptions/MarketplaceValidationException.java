2667037ffffe7fedd9d2a8a14634c9786f1ef330
Michal <michal.grubski@ts.fujitsu.com>
2017-07-21 15:14:06 +0200

    #742 Added missing constructors

--- oscm-extsvc/javasrc/org/oscm/types/exceptions/MarketplaceValidationException.java
+++ oscm-extsvc/javasrc/org/oscm/types/exceptions/MarketplaceValidationException.java
@@ -5,16 +5,74 @@
  *******************************************************************************/
 package org.oscm.types.exceptions;
 
+import javax.xml.ws.WebFault;
+
+import org.oscm.types.exceptions.beans.ApplicationExceptionBean;
+
 /**
  * Created by PLGrubskiM on 2017-07-21.
  */
+@WebFault(name = "MarketplaceValidationException", targetNamespace = "http://oscm.org/xsd")
 public class MarketplaceValidationException extends SaaSApplicationException{
 
+    /**
+     * Constructs a new exception with <code>null</code> as its detail message.
+     * The cause is not initialized.
+     */
     public MarketplaceValidationException() {
-        super();
+
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param message
+     *            the detail message
+     * @param cause
+     *            the cause
+     */
+    public MarketplaceValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message. The cause
+     * is not initialized.
+     *
+     * @param message
+     *            the detail message
+     */
     public MarketplaceValidationException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and bean for
+     * JAX-WS exception serialization.
+     *
+     * @param message
+     *            the detail message
+     * @param bean
+     *            the bean for JAX-WS exception serialization
+     */
+    public MarketplaceValidationException(String message,
+                                       ApplicationExceptionBean bean) {
+        super(message, bean);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message, cause, and
+     * bean for JAX-WS exception serialization.
+     *
+     * @param message
+     *            the detail message
+     * @param bean
+     *            the bean for JAX-WS exception serialization
+     * @param cause
+     *            the cause
+     */
+    public MarketplaceValidationException(String message,
+                                       ApplicationExceptionBean bean, Throwable cause) {
+        super(message, bean, cause);
     }
 }
