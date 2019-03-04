3a99555bc8d65a1f864bd1ac686ebcd889ec0c1c
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-03-04 15:10:51 +0100

    Updated ws test config to run without initial env setup

---
+++ oscm-webservices-tests/javasrc/org/oscm/ws/WSClient.java
@@ -0,0 +1,47 @@
+package org.oscm.ws;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+
+import org.junit.Test;
+import org.oscm.intf.IdentityService;
+
+public class WSClient {
+    
+    @Test
+    public void testClient() throws MalformedURLException{
+        System.out.println("Starting WS client app");
+
+        //https://g02dexxnow29066.g02.fujitsu.local:8181/oscm/IdentityService/BASIC?wsdl
+
+        String baseHttp = "http://g02dexxnow29066.g02.fujitsu.local:8180";
+        String serviceName = "IdentityService";
+        String authType = "BASIC";
+
+        String wsdlUrl = baseHttp + "/oscm/" + serviceName + "/" + authType + "?wsdl";
+
+        System.out.println(wsdlUrl);
+
+        URL url = new URL(wsdlUrl);
+        QName qName = new QName("http://oscm.org/xsd", serviceName);
+        Service service = Service.create(url, qName);
+
+        IdentityService identityService = service.getPort(IdentityService.class);
+
+
+        BindingProvider bindingProvider = (BindingProvider) identityService;
+        Map<String, Object> clientRequestContext = bindingProvider.getRequestContext();
+
+        clientRequestContext.put(BindingProvider.USERNAME_PROPERTY, "administrator");
+        clientRequestContext.put(BindingProvider.PASSWORD_PROPERTY, "admin123");
+        clientRequestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, baseHttp + "/" + serviceName + "/" + authType);
+
+        System.out.println(identityService.getCurrentUserDetails());
+    }
+
+}
