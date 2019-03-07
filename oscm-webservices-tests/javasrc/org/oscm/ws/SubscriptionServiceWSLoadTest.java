3f2d98d3d32a07c5f5c1c25f80f98419a5c58277
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-03-07 11:06:02 +0100

    Final load test scenario

--- oscm-webservices-tests/javasrc/org/oscm/ws/SubscriptionServiceWSLoadTest.java
+++ oscm-webservices-tests/javasrc/org/oscm/ws/SubscriptionServiceWSLoadTest.java
@@ -1,70 +1,126 @@
 package org.oscm.ws;
 
-import static org.junit.Assert.*;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.oscm.intf.ServiceProvisioningService;
 import org.oscm.intf.SubscriptionService;
-import org.oscm.types.exceptions.OrganizationAuthoritiesException;
+import org.oscm.types.exceptions.ObjectNotFoundException;
+import org.oscm.vo.VOParameter;
 import org.oscm.vo.VOService;
 import org.oscm.vo.VOSubscription;
+import org.oscm.vo.VOSubscriptionDetails;
+import org.oscm.vo.VOUda;
 import org.oscm.ws.base.ServiceFactory;
 import org.oscm.ws.base.WebserviceTestBase;
 
 public class SubscriptionServiceWSLoadTest {
-    
-    private static ServiceProvisioningService serviceProvisioningService;
-    private static SubscriptionService subscriptionService;
-    
-    @BeforeClass
-    public static void setup() throws Exception{
-        
-        long userKey = 10000;
-        
-        serviceProvisioningService = ServiceFactory.getDefault()
-                .getServiceProvisioningService(String.valueOf(userKey), WebserviceTestBase.DEFAULT_PASSWORD);
-        
-        subscriptionService = ServiceFactory.getDefault()
-                .getSubscriptionService(String.valueOf(userKey), WebserviceTestBase.DEFAULT_PASSWORD);
-        
-        //VOSubscription subscription = createSubscription();
-        //subscriptionService.subscribeToService(subscription, service, users, paymentInfo, billingContact, udas)
-    }
-    
-    @Test
-    public void testSubscriptionIdentifiers() throws OrganizationAuthoritiesException{
-        
-        List<String> identifiers = subscriptionService.getSubscriptionIdentifiers();
-        System.out.println(identifiers);
-    }
-    
-    @Test
-    public void testProvisioningService(){
-        
-        String mpId = "d50e0266";
-        List<VOService> services = serviceProvisioningService.getServicesForMarketplace(mpId);
-        int size = services.size();
-        assertEquals("Wrong number of services:"+ size,2,size);  
-        
-        for(VOService service:services){
-            System.out.println(service.getKey());
-            System.out.println(service.getServiceId());
-            System.out.println(service.getName());
-            System.out.println(service.getTechnicalId());
-        }
-    }
-    
-    private VOSubscription createSubscription() {
-   
-        VOSubscription subscription = new VOSubscription();
-        
-        String subscriptionId = "LoadSub_"+Long.toHexString(System.currentTimeMillis());
-        subscription.setSubscriptionId(subscriptionId);
 
-        return subscription;
+  private static ServiceProvisioningService serviceProvisioningService;
+  private static SubscriptionService subscriptionService;
+
+  private static final String SUBSCRIPTION_ID_PREFIX = "ZZZ_";
+  private static final String MARKETPLACE_ID = "99512989";
+  private static final String SERVICE_ID = "AppSampleId";
+
+  private static final long NO_OF_SUBS = 200;
+  private static final long USER_KEY = 10000;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+
+    serviceProvisioningService =
+        ServiceFactory.getDefault()
+            .getServiceProvisioningService(
+                String.valueOf(USER_KEY), WebserviceTestBase.DEFAULT_PASSWORD);
+
+    subscriptionService =
+        ServiceFactory.getDefault()
+            .getSubscriptionService(String.valueOf(USER_KEY), WebserviceTestBase.DEFAULT_PASSWORD);
+  }
+
+  @Test
+  public void testCreateAndModifySubscriptions() throws Exception {
+
+    for (int i = 0; i < NO_OF_SUBS; i++) {
+      createSubscription(SUBSCRIPTION_ID_PREFIX + i);
     }
 
+    System.out.println("Creation of " + NO_OF_SUBS + "subscriptions finished");
+    System.out.println("Waiting for background asynchronous task to be completed");
+    Thread.sleep(120000);
+
+    for (int i = 0; i < NO_OF_SUBS; i++) {
+      modifySubscription(SUBSCRIPTION_ID_PREFIX + i);
+    }
+    System.out.println("Modification of " + NO_OF_SUBS + "subscriptions finished");
+  }
+
+  public VOSubscription modifySubscription(String subscriptionId) throws Exception {
+
+    VOSubscriptionDetails subscription = subscriptionService.getSubscriptionDetails(subscriptionId);
+    List<VOParameter> parameters = subscription.getSubscribedService().getParameters();
+
+    List<VOParameter> updatedParams =
+        parameters
+            .stream()
+            .filter(param -> param.getParameterDefinition().getParameterId().equals("PARAM_EMAIL"))
+            .map(
+                param -> {
+                  param.setValue("changedmail@fujitsu.com");
+                  return param;
+                })
+            .collect(Collectors.toList());
+
+    VOSubscriptionDetails modifiedSubscription =
+        subscriptionService.modifySubscription(subscription, updatedParams, new ArrayList<VOUda>());
+
+    return modifiedSubscription;
+  }
+
+  public VOSubscription createSubscription(String subscriptionId) throws Exception {
+
+    VOSubscription subscription = prepareSubscription(subscriptionId, false);
+    VOService sampleService = getSampleService(MARKETPLACE_ID, SERVICE_ID);
+    VOSubscription createdSubscription =
+        subscriptionService.subscribeToService(
+            subscription, sampleService, null, null, null, new ArrayList<VOUda>());
+    return createdSubscription;
+  }
+
+  private static VOService getSampleService(String mpId, String serviceId)
+      throws ObjectNotFoundException {
+
+    List<VOService> services = serviceProvisioningService.getServicesForMarketplace(mpId);
+
+    for (VOService service : services) {
+      if (serviceId.equals(service.getServiceId())) {
+        return service;
+      }
+    }
+
+    throw new ObjectNotFoundException(
+        "Service[" + serviceId + "] not found in marketplace[" + mpId + "]");
+  }
+
+  private static VOSubscription prepareSubscription(String subscriptionId, boolean dateTimeSuffix) {
+
+    VOSubscription subscription = new VOSubscription();
+
+    LocalDateTime localDateTime = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+    String dateTime = localDateTime.format(formatter);
+
+    if (dateTimeSuffix) {
+      subscriptionId += "_" + dateTime;
+    }
+    subscription.setSubscriptionId(subscriptionId);
+
+    return subscription;
+  }
 }
